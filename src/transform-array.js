7ff5064aa6772bc187ec67f6c3a122b95b328c49
aimcode <aimcode2020@gmail.com>
2020-09-28 23:19:57 +0500

    feat: add solution for transform array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,23 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform( arr ) {
+  let newArr = arr.slice()
+
+    for(let i = 0; i < newArr.length; i++) {
+        if(newArr[i] === '--double-next') {
+            newArr[i] = newArr[i + 1 ]
+        } else if (newArr[i] === '--double-prev') {
+            newArr[i] = newArr[ i - 1]
+        } else if(newArr[i] === '--discard-prev') {
+            newArr[i - 1] = undefined
+            newArr[i] = undefined
+        } else if (newArr[i] === '--discard-next') {
+            newArr[i] = undefined
+            newArr[ i + 1] = undefined
+        }
+    }
+
+    return newArr.filter(el => {
+        return (el !== undefined)
+    })
 };
