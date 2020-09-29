8d914a9d84d25b71f97aca2451373400543d337b
aimcode <aimcode2020@gmail.com>
2020-09-29 19:33:01 +0500

    feat: add solution for extended repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,37 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let {
+    repeatTimes,
+    separator = "+",
+    addition,
+    additionRepeatTimes,
+    additionSeparator = "|",
+  } = options;
+
+  let additionResult = [];
+
+  if (addition !== undefined) {
+    for (let j = 0; j < additionRepeatTimes; j++) {
+      additionResult[j] = addition + "";
+    }
+
+    if (additionRepeatTimes !== undefined) {
+      additionResult = additionResult.join(`${additionSeparator}`);
+    } else {
+      additionResult = addition;
+    }
+  }
+
+  let resStr = str + additionResult;
+  let result = [];
+  if (repeatTimes !== undefined) {
+    for (let i = 0; i <= repeatTimes - 1; i++) {
+      result[i] = resStr + "";
+    }
+  } else {
+    result[0] = resStr;
+  }
+
+  return result.join(`${separator}`);
 };
-  
